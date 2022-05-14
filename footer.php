23a523de63b6a3157dd93956928744bc7e352bef
Lukas Niebler <nieblerlukas@gmail.com>
2022-05-14 10:59:24 +0200

    Small improvements in header and footer

--- footer.php
+++ footer.php
@@ -12,17 +12,9 @@
 ?>
 	<div class="footer-wrapper">
 	<footer id="colophon" class="site-footer center">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ribonucleic' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ribonucleic' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ribonucleic' ), 'ribonucleic', '<a href="http://www.lukas-niebler.de">Lukas Niebler</a>' );
+				printf( esc_html__( 'Theme by %2$s', 'ribonucleic' ), 'ribonucleic', '<a href="http://www.lukas-niebler.de">Lukas Niebler</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
